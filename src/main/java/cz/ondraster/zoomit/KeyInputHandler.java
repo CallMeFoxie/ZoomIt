97edee85813526a79933f9670b7c6a54e0ab846d
Ondra Moravek <ondra.moravek@hotmail.com>
2014-07-31 16:42:21 +0200

    Fix for obf environment and for mouse zoom

--- src/main/java/cz/ondraster/zoomit/KeyInputHandler.java
+++ src/main/java/cz/ondraster/zoomit/KeyInputHandler.java
@@ -7,17 +7,32 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class KeyInputHandler {
+   boolean previousDoZoom = false;
+
    @SubscribeEvent
    public void onPreRender(TickEvent.RenderTickEvent event) {
-      int key = ReflectionHelper.getPrivateValue(KeyBinding.class, ZoomIt.keyBinding, "keyCode");
-      if (Keyboard.isKeyDown(key)) {
-         ReflectionHelper.setPrivateValue(EntityRenderer.class, Minecraft.getMinecraft().entityRenderer, ZoomIt.zoomMultiplier, "cameraZoom");
-         Minecraft.getMinecraft().gameSettings.smoothCamera = true;
+      int key = ReflectionHelper.getPrivateValue(KeyBinding.class, ZoomIt.keyBinding, "keyCode", "field_74512_d");
+      boolean doZoom = false;
+
+      if (key < 0) {
+         if (Mouse.isButtonDown(key + 100))
+            doZoom = true;
       } else {
-         ReflectionHelper.setPrivateValue(EntityRenderer.class, Minecraft.getMinecraft().entityRenderer, 1.0d, "cameraZoom");
+         if (Keyboard.isKeyDown(key))
+            doZoom = true;
+      }
+
+      if (doZoom && !previousDoZoom) {
+         ReflectionHelper.setPrivateValue(EntityRenderer.class, Minecraft.getMinecraft().entityRenderer, ZoomIt.zoomMultiplier, "cameraZoom", "field_78503_V");
+         Minecraft.getMinecraft().gameSettings.smoothCamera = true;
+      } else if (!doZoom && previousDoZoom) {
+         ReflectionHelper.setPrivateValue(EntityRenderer.class, Minecraft.getMinecraft().entityRenderer, 1.0d, "cameraZoom", "field_78503_V");
          Minecraft.getMinecraft().gameSettings.smoothCamera = false;
       }
+
+      previousDoZoom = doZoom;
    }
 }
